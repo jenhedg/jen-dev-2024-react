9126d458bf820dedd2721a5626061d383c3f1c7e
Jen Hedg <jennifernhf@gmail.com>
2024-07-16 20:59:04 -0700

    styles

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -11,7 +11,7 @@ import '../../components/Resume/Resume.css';
 export default function Resume() {
   return (
     <article className="resume">
-      <div className="resume-intro">
+      <div className="resume-header">
         <h2 className="resume-title">Jennifer Hedgcock</h2>
         <ul className="contact-list">
           <li className="contact-item">
@@ -38,10 +38,11 @@ export default function Resume() {
       </div>
 
       <div className="resume-inner">
-        <article className="position">
-          <h3><span>CMS Developer,</span> Simpleview</h3>
-          <p><b><i>December 2020 - present</i></b></p>
-          <ul className="responsibilities">
+        <article className="resume-role">
+          <h3 className="resume-title">CMS Developer</h3>
+          <h4 className="resume-company"> Simpleview</h4>
+          <p className="resume-dates"><b><i>December 2020 - present</i></b></p>
+          <ul className="resp-list">
             <li>Implement and maintain database-driven CMS product applications, integrating with various APIs and databases</li>
             <li>Strategically plan and execute projects within allocated timelines and budget constraints</li>
             <li>Complete work autonomously, collaborating as needed with teammates, lead developer, project manager and XD team.</li>
@@ -50,10 +51,10 @@ export default function Resume() {
             <li>Create and maintain documentation for system features and modules in Confluence.</li>
           </ul>
         </article>
-        <article className="position">
+        <article className="role">
           <h3><span>Front-End Developer,</span> PetSmart</h3>
           <span><b><i>August 2020 - December 2020</i></b></span>
-          <ul className="responsibilities">
+          <ul className="resp-list">
             <li>Partnered with client-led and internal teams, including offshore QA teams. </li>
             <li>Programmed enterprise-level webapps, microsites and marketing tools.</li>
             <li>Self-led SCSS and front end build tooling refactoring project</li>
@@ -62,26 +63,26 @@ export default function Resume() {
             <li>Created documentation and style guides for both clients and internal teams.</li>
           </ul>
         </article>
-        <article className="position">
+        <article className="role">
           <h3><span>Lead Front-End Developer,</span> Crescendo Collective</h3>
           <span><b><i>February 2016 - Mar 2020</i></b></span>
-          <ul className="responsibilities">
+          <ul className="resp-list">
             <li>Programmed enterprise-level CMS webapps, microsites and marketing tools</li>
             <li>Collaborated with client-led, internal and offshore QA teams</li>
             <li>Led daily/weekly releases and deployments</li>
           </ul>
         </article>
-        <article className="position">
+        <article className="role">
           <h3><span>Spanish Tutor,</span>- Wyzant</h3>
           <span><b><i>January 2015 - January 2016</i></b></span>
-          <ul className="responsibilities">
+          <ul className="resp-list">
             <li>Tutored K-12 and college students with Spanish coursework and test preparation</li>
           </ul>
         </article>
-        <article className="position">
+        <article className="role">
           <h3><span>Lab Assistant</span>- Gemological Institute of America</h3>
           <span><b><i>July 2013 - August 2014</i></b></span>
-          <ul className="responsibilities">
+          <ul className="resp-list">
             <li>Processed 400+ precious stones daily using various diagnostic testing technologies</li>
             <li>Supported research team to develop and test proprietary diagnostic technologies</li>
           </ul>
